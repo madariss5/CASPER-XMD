ee7c60aa6ec265500fe6c34eeee4183997b95186
Casper-Tech-ke <caspertechke@gmail.com>
2025-02-20 19:08:27 +0300

    𝙲𝙰𝚂𝙿𝙴𝚁-𝚃𝙴𝙲𝙷

--- plugins/Owner/restart.js
+++ plugins/Owner/restart.js
@@ -5,7 +5,7 @@ module.exports = {
   command: ['restart'],
   operate: async ({ Cypher, m, reply, isCreator, mess }) => {
     if (!isCreator) return reply(mess.owner);
-    reply(`*Restarting...*`);
+    reply(`*Restarting CASPER-XMD...*`);
     await sleep(3000);
     process.exit(0);
   }
