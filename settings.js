a131e6aa3f7dc9336a64932b217110b691269965
Casper-Tech-ke <caspertechke@gmail.com>
2025-02-20 18:39:18 +0300

    𝙲𝙰𝚂𝙿𝙴𝚁-𝚃𝙴𝙲𝙷

--- settings.js
+++ settings.js
@@ -1,7 +1,11 @@
-//project_name : CASPER-XMD
-// @author : Casper-Tech-ke
-// @whatsapp : +254754783972
+  //project_name : CASPER-XMD 
+// @author : TRABY
+// @youtube : https://www.youtube.com/@casper.tech.254
+// @instagram : samchris.kym.official
+// @telegram : t.me/+254732982940
 // @github : Casper-Tech-ke
+// @tiktok : Traby_qriz
+// @whatsapp : +254732982940
 //*
 //* 
 //=================================================//
@@ -12,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //=================================================//
 global.SESSION_ID = process.env.SESSION_ID || '' 
-//Enter your Casper session id here; must start with CASPER-TECH:~
+//Enter your CASPER-XMD session id here; must start with CASPER-TECH:~
 
 //=================================================//
 global.botname = process.env.BOT_NAME || 'CASPER-XMD' 
@@ -28,30 +32,33 @@ global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['254732982940'];
 //NB: They'll be able to use every functions of the bot without restrictions.
 
 //=================================================//
-global.ownername = process.env.OWNER_NAME || 'Casper-Tech-ke' 
+global.ownername = process.env.OWNER_NAME || 'TRABY' 
 //Type your name here
 
 //=================================================//
-global.plink = process.env.PLINK || "https://github.com/Casper-Tech-ke"
+global.plink = process.env.PLINK || "https://www.facebook.com/samchris.officiall"
 
 //=================================================//
-global.wm = process.env.GL_WM || "©CASPER-XMD"
+global.wm = process.env.GL_WM || "©CASPER"
 
 //=================================================//
-global.packname = process.env.STICKER_PACK_NAME || "CASPER" 
+global.packname = process.env.STICKER_PACK_NAME || "Casper" 
 //The sticker pack name
 
 //=================================================//
-global.author = process.env.STICKER_AUTHOR_NAME || "CASPER-TECH" 
+global.author = process.env.STICKER_AUTHOR_NAME || "Traby" 
 //The sticker author name
 
 //=================================================//
-global.prefixz = process.env.PREFIX || '.'
+global.prefixz = process.env.BOT_PREFIX || '.'
 //Set your desired prefix
 
 //=================================================//
 global.mode = process.env.MODE || 'public';
-// Set to 'private' to enable private mode, otherwise default is 'public'
+// Set 'private' to enable private mode
+// Set 'public' to enable public mode
+// Set 'group' to enable only group
+// Set 'pm' to enable only private chats
 
 //=================================================//
 global.statusemoji = process.env.STATUS_EMOJI || '🩷'
@@ -69,16 +76,24 @@ global.autoreactstatus = process.env.AUTO_STATUS_REACT || 'true'
 global.alwaysonline = process.env.ALWAYS_ONLINE || 'true'
 //Set true to make the bot online 24/7 or set false to disable always online
 
+
 //=================================================//
 global.chatbot = process.env.CHATBOT || 'false'
 // set true to enable and false to disable auto ai chatbot
 
 //=================================================//
-global.antidelete = process.env.ANTIDELETE || 'private'
+global.antidelete = process.env.ANTIDELETE || 'chat'
 // options:- 'private', 'chat' or 'off'
 // private = Sends to message yourself 
 // chat = sends to the current chat 
-// off = Disables
+// off = Disables detection of deleted messages
+
+//=================================================//
+global.antiedit = process.env.ANTI_EDIT || 'private'
+// options:- 'private', 'chat' or 'off'
+// private = Sends to message yourself 
+// chat = sends to the current chat 
+// off = Disables detection of edited messages
 
 //=================================================//
 global.anticall = process.env.ANTI_CALL || 'false'
@@ -97,7 +112,7 @@ global.autoread = process.env.AUTO_READ || 'false';
 // Set to 'true' to enable automatic reading of messages
 
 //=================================================//
-global.menustyle = process.env.MENU_STYLE || '2' 
+global.menustyle = process.env.MENU_STYLE || '3' 
 // options 1, 2, 3, 4 or 5
 // 1 = Document menu(Android only)
 // 2 = Text only menu(Android & iOS)
@@ -115,6 +130,10 @@ global.mess = {
   admin: '*You will unlock this feature with me as an admin!*', 
   notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
 }
+//=================================================//
+
+//=================================================//
+
 //=================================================//
 
 //=================================================//
